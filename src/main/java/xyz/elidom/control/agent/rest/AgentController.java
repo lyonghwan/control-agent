f63a897771e5fb0cef9c47a53712ad8ae9967f89
lyonghwan <longhuan@hatiolab.com>
2017-03-11 23:11:23 +0900

    로그 마지막 라인 읽기, 로그 다운로드

--- src/main/java/xyz/elidom/control/agent/rest/AgentController.java
+++ src/main/java/xyz/elidom/control/agent/rest/AgentController.java
@@ -1,17 +1,29 @@
 package xyz.elidom.control.agent.rest;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-//import org.json.JSONArray;
-//import org.json.JSONObject;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.input.ReversedLinesFileReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +33,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,17 +41,17 @@ import xyz.elidom.control.agent.util.StreamPrinter;
 
 @RestController
 public class AgentController {
-	
+
 	protected Logger logger = LoggerFactory.getLogger(AgentController.class);
 
 	private static final String FILE_SEPARATOR = "/";
 	private static final String LOG_FILENAME = "applicatoin.";
 	private static final String LOG_FILENAME_2 = "application.";
 	private static final String LOG_FILE_EXT = ".log";
-	
+
 	@Autowired
 	Environment env;
-	 
+
 	/**
 	 * Application Start
 	 * 
@@ -47,18 +60,19 @@ public class AgentController {
 	 */
 	@RequestMapping(value = "/apps/{app_id}/start", method = RequestMethod.POST)
 	public String startBoot(@PathVariable("app_id") String appId) {
-		HashMap<String,String> pMap = this.checkProperties(appId, "start");
-		if(pMap.get("RESULT").equals("FAIL")) return pMap.get("MSG");
-		
+		HashMap<String, String> pMap = this.checkProperties(appId, "start");
+		if (pMap.get("RESULT").equals("FAIL"))
+			return pMap.get("MSG");
+
 		try {
 			this.commandStart(pMap.get("PATH"));
 		} catch (Exception e) {
 			return "Error : \n\n" + e.getMessage();
 		}
-		
+
 		return "Enterd Startup Command SUCCESS";
 	}
-	
+
 	/**
 	 * Application Restart
 	 * 
@@ -72,15 +86,15 @@ public class AgentController {
 		} catch (Exception e) {
 			this.logger.error("Failed to shutdown application : " + e.getMessage());
 		}
-		
+
 		try {
 			Thread.sleep(10000);
-		} catch (Exception e) {			
+		} catch (Exception e) {
 		}
-		
+
 		return this.startBoot(appId);
 	}
-	
+
 	/**
 	 * Application Stop
 	 * 
@@ -95,7 +109,7 @@ public class AgentController {
 		ResponseEntity<String> response = rest.postForEntity(url, "", String.class);
 		return response.getBody();
 	}
-	
+
 	/**
 	 * Application Deploy
 	 * 
@@ -105,86 +119,168 @@ public class AgentController {
 	@RequestMapping(value = "/apps/{app_id}/udpate", method = RequestMethod.POST)
 	public String deploy(@PathVariable("app_id") String appId) {
 		this.stopBoot(appId);
-		
+
 		try {
 			Thread.sleep(10000);
-		} catch (Exception e) {			
-		}		
-		
-		HashMap<String,String> pMap = this.checkProperties(appId, "update");
-		if(pMap.get("RESULT").equals("FAIL")) return pMap.get("MSG");
-		
+		} catch (Exception e) {
+		}
+
+		HashMap<String, String> pMap = this.checkProperties(appId, "update");
+		if (pMap.get("RESULT").equals("FAIL"))
+			return pMap.get("MSG");
+
 		try {
 			this.commandStart(pMap.get("PATH"));
 		} catch (Exception e) {
 			return "Failed to update application execution file : " + e.getMessage();
 		}
-		
+
 		this.startBoot(appId);
 		return "OK";
 	}
-	
+
 	/**
-	 * Log File Path를 리턴 
+	 * Log File Path를 리턴
 	 * 
-	 * @param path
+	 * @param appId
 	 * @return
 	 */
-	private String getLogFilePath(String path) {
-		StringBuffer logPath = new StringBuffer();
+	private String getLogFilePath(String appId) {
+		HashMap<String, String> pMap = this.checkProperties(appId, "log");
+		
+		if (pMap.get("RESULT").equals("FAIL"))
+			throw new RuntimeException(pMap.get("MSG"));
+
+		String path = pMap.get("PATH");
 		Date today = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		logPath.append(path).append(path.endsWith(FILE_SEPARATOR) ? "" : FILE_SEPARATOR).append(LOG_FILENAME).append(sdf.format(today)).append(LOG_FILE_EXT);
 		
+		StringBuffer logPath = new StringBuffer();
+		logPath.append(path).append(path.endsWith(FILE_SEPARATOR) ? "" : FILE_SEPARATOR).append(LOG_FILENAME)
+				.append(sdf.format(today)).append(LOG_FILE_EXT);
+
 		File file = new File(logPath.toString());
-		if(!file.exists()) {
+		if (!file.exists()) {
 			logPath = new StringBuffer();
-			logPath.append(path).append(path.endsWith(FILE_SEPARATOR) ? "" : FILE_SEPARATOR).append(LOG_FILENAME_2).append(sdf.format(today)).append(LOG_FILE_EXT);
+			logPath.append(path).append(path.endsWith(FILE_SEPARATOR) ? "" : FILE_SEPARATOR).append(LOG_FILENAME_2)
+					.append(sdf.format(today)).append(LOG_FILE_EXT);
 			file = new File(logPath.toString());
-			
-			if(!file.exists()) {
+
+			if (!file.exists()) {
 				throw new RuntimeException("Log File (" + logPath.toString() + ") Not Found!");
 			}
 		}
-		
+
 		return logPath.toString();
+	}
+
+	/**
+	 * 로그 파일을 읽어서 내용을 리턴
+	 * 
+	 * @param appId
+	 * @param lines
+	 * @return 오늘의 로그의 내용을 리턴 
+	 */
+	@RequestMapping(value = "/apps/{app_id}/log", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, String> readLog(
+			@PathVariable("app_id") String appId,
+			@RequestParam(name = "lines", required = false) Integer lines) {
+		
+		String logPath = this.getLogFilePath(appId);
+		String content = (lines == null || lines == 0) ? 
+						 this.readAllLines(logPath) : 
+						 this.readLastLines(new File(logPath), lines);
+
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("id", "1");
+		result.put("log", content);
+		return result;
 	}
 	
 	/**
-	 * 로그 파일을 읽어서 내용을 리턴  
+	 * 로그 파일을 다운로드 
 	 * 
 	 * @param appId
-	 * @return today log file
+	 * @return 오늘의 로그의 내용을 리턴 
 	 */
-	@RequestMapping(value = "/apps/{app_id}/log", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public Map<String, String> readLog(@PathVariable("app_id") String appId) {
+	@RequestMapping(value = "/apps/{app_id}/download_log", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public boolean downloadLog(HttpServletRequest req, HttpServletResponse res, @PathVariable("app_id") String appId) {
+		String logPath = this.getLogFilePath(appId);
+		File file = new File(logPath);
 		
-		HashMap<String,String> pMap = this.checkProperties(appId, "log");
-		if(pMap.get("RESULT").equals("FAIL")) return pMap;
+		res.setCharacterEncoding("UTF-8");
+		res.setContentType("text/plain;charset=UTF-8");
+		res.addHeader("Content-Type", "application/octet-stream");
+		res.addHeader("Content-Transfer-Encoding", "binary;");
+		res.addHeader("Content-Length", Long.toString(file.length()));
+		res.setHeader("Pragma", "cache");
+		res.setHeader("Cache-Control", "public");
 		
-		String logPath = this.getLogFilePath(pMap.get("PATH")); 
+		ServletOutputStream outStream = null;
+		ByteArrayInputStream inStream = null;
+		byte[] buffer = new byte[4096];
+		
+		try {
+			res.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(file.getName(), "UTF-8"));
+			outStream = res.getOutputStream();
+			inStream = new ByteArrayInputStream(FileUtils.readFileToByteArray(file));			
+			int byteCount = 0;
+			
+			do {
+				byteCount = inStream.read(buffer);
+				if(byteCount == -1) {
+					break;
+				}
+				
+				outStream.write(buffer, 0, byteCount);
+				outStream.flush();
+			} while(true);
+
+		} catch (Exception e) {
+			throw new RuntimeException("File Download Exception!", e);
+			
+		} finally {
+			try {
+				inStream.close();
+			} catch (IOException e) {
+			}
+			
+			try {
+				outStream.close();
+			} catch (IOException e) {
+			}
+		}
+		
+		return true;		
+	}
+	
+	/**
+	 * 파일의 모든 라인 읽어 리턴 
+	 * 
+	 * @param logPath
+	 * @return
+	 */
+	private String readAllLines(String logPath) {
 		StringBuffer content = new StringBuffer();
 		FileReader fReader = null;
 		BufferedReader bReader = null;
-		Map<String, String> result = new HashMap<String, String>();
-		
-		// TODO 마지막 1000 라인만 읽기 - 읽을 라인은 설정으로 ...
+
 		try {
 			fReader = new FileReader(logPath);
 			bReader = new BufferedReader(fReader);
-			String temp = "";
+			String temp = null;
+			
 			while((temp = bReader.readLine()) != null) {
 			    content.append(temp).append("\n");
 			}
+			
+			return content.toString();
+			
 		} catch(FileNotFoundException e) {
-			result.put("success", "false");
-			result.put("msg", "Log File Not Found!");
-			return result;
+			throw new RuntimeException("Log file not found!", e);
 			
 		} catch (Exception e) {
-			result.put("success", "false");
-			result.put("msg", e.getMessage());
-			return result;
+			throw new RuntimeException("Log file not found!", e);
 			
 		} finally {
 			if(bReader != null) {
@@ -200,200 +296,238 @@ public class AgentController {
 				}
 			}
 		}
-		
-		result.put("id", "1");
-		result.put("log", content.toString());
-		return result;
 	}
 	
 	/**
-	 * ping & Pong 
+	 * 마지막 lines 라인을 읽어 리턴 
+	 * 
+	 * @param filePath
+	 * @param lines
+	 * @return
+	 */
+	private String readLastLines(File file, int lines) {
+		StringBuilder builder = new StringBuilder();
+		ReversedLinesFileReader rlfr = null;
+		
+		try {
+			rlfr = new ReversedLinesFileReader(file, Charset.forName("UTF-8"));
+			String temp = null;
+			int counter = 0;
+
+			while(counter < lines) {
+				temp = rlfr.readLine();
+				if(temp != null) {
+					builder.insert(0, temp + "\n");
+					counter++;
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to read log file!", e);
+			
+		} finally {
+			try {
+				rlfr.close();
+			} catch (IOException e) {
+			}
+		}
+		
+		return builder.toString();
+	}
+
+	/**
+	 * ping & pong
 	 */
 	@RequestMapping(value = "/ping", method = RequestMethod.GET)
 	public String ping() {
 		return "pong";
 	}
-	
-	/*@RequestMapping(value = "/apps/{app_id}/info", method = RequestMethod.POST)
-	public String getOneDomainInfo(@PathVariable("app_id") String appId) {
-		HashMap<String, String> retMap = this.getDomainInfo(appId);
-		if(retMap.get("RESULT").equals("FAIL")) return retMap.get("MSG");
-		return retMap.get("RES_JSON");
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/apps/{app_id}/info", method = RequestMethod.GET)
+	public Map<String, Object> getOneDomainInfo(@PathVariable("app_id") String appId) {
+		Map<String, Object> appInfo = (Map<String, Object>) this.getAppInfo(appId);
+
+		if (appInfo.get("RESULT").equals("FAIL")) {
+			return appInfo;
+		}
+
+		return appInfo;
 	}
-	
-	@RequestMapping(value = "/apps/infos", method = RequestMethod.GET)
-	public String getDomainInfos()  {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@RequestMapping(value = "/apps/infos", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> getAppInfos() {
 		String[] ids = this.getPropertiesByKey(this.env, "apps.id");
-		
-		if(ids == null || ids.length == 0) {
-			return "Domain Id Not Found !";
+		Map<String, Object> result = new HashMap<String, Object>();
+
+		if (ids == null || ids.length == 0) {
+			result.put("RESULT", "FAIL");
+			result.put("MSG", "Application Id Not Found !");
+			return result;
 		}
-		
-		
-		JSONArray array = new JSONArray();
-		
-		for(int i = 0 ; i < ids.length ; i++) {
-			HashMap<String, String> retMap = this.getDomainInfo(ids[i]);
-			if(retMap.get("RESULT").equals("FAIL")) return retMap.get("MSG");
-			array.put(new JSONObject(retMap.get("RES_JSON")));
+
+		List apps = new ArrayList();
+
+		for (int i = 0; i < ids.length; i++) {
+			Map<String, Object> itemObj = (Map<String, Object>) this.getAppInfo(ids[i]);
+			if (itemObj.get("RESULT").equals("FAIL")) {
+				return itemObj;
+			}
+
+			apps.add(itemObj.get("RES_JSON"));
 		}
-		
-		JSONObject resObj = new JSONObject();
-		resObj.put("items", array);
-		return resObj.toString();
+
+		result.put("items", apps);
+		return result;
 	}
-	
-	private HashMap<String, String> getDomainInfo(String app_id) {
-		HashMap<String,String> pMap = this.checkProperties(app_id, "info");
-		if(pMap.get("RESULT").equals("FAIL")) return pMap;
-		
+
+	private Object getAppInfo(String appId) {
+		Map<String, String> pMap = this.checkProperties(appId, "info");
+		if (pMap.get("RESULT").equals("FAIL")) {
+			return pMap;
+		}
+
 		String url = "http://localhost:" + pMap.get("PORT") + "/info";
 		BufferedReader brIn = null;
 		InputStreamReader isr = null;
 		StringBuffer resStr = null;
-		
+
 		try {
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 			int responseCode = con.getResponseCode();
-			
-			if(responseCode == 401 ){
+
+			if (responseCode == 401) {
 				pMap.put("RESULT", "FAIL");
 				pMap.put("MSG", "Unauthorized Request !");
 				return pMap;
 			}
-			
+
 			isr = new InputStreamReader(con.getInputStream());
 			brIn = new BufferedReader(isr);
-			
+
 			String inputLine;
 			resStr = new StringBuffer();
 
 			while ((inputLine = brIn.readLine()) != null) {
 				resStr.append(inputLine);
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			pMap.put("RESULT", "FAIL");
 			pMap.put("MSG", "Error : \n\n" + e.getMessage());
 			return pMap;
-			
+
 		} finally {
-			if(brIn != null) {
+			if (brIn != null) {
 				try {
 					brIn.close();
 				} catch (Exception e) {
 				}
 			}
-			if(isr != null) {
+			if (isr != null) {
 				try {
 					isr.close();
 				} catch (Exception e) {
 				}
 			}
 		}
-		
-		JSONObject resJson = new JSONObject(resStr.toString());
-		resJson.put("id", app_id);
-		pMap.put("RES_JSON", resJson.toString());
+
+		pMap.put("RES_JSON", resStr.toString());
 		return pMap;
-	}*/
+	}
 
 	/**
-	 * Command 실행 
+	 * Command 실행
 	 * 
-	 * @param Path
+	 * @param path
 	 * @throws Exception
 	 */
-	private void commandStart(String Path) throws Exception{
+	private void commandStart(String path) throws Exception {
 		ProcessBuilder pBuilder = new ProcessBuilder();
-		pBuilder.command(Path);
+		pBuilder.command(path);
 		Process process = pBuilder.start();
 		StreamPrinter.printStream(process);
-		
-		while(process.isAlive()) {
+
+		while (process.isAlive()) {
 			Thread.sleep(100);
 		}
 	}
-	
+
 	/**
-	 * appId별 actionCode에 대응하는 프로퍼티를 찾아 리턴  
+	 * appId별 actionCode에 대응하는 프로퍼티를 찾아 리턴
 	 * 
-	 * @param appId 
+	 * @param appId
 	 * @param actionCode
-	 * @return HashMap
-	 *         RESULT : 성공 혹은 실패 
-	 *         MSG : 실패시 에러 메시지 
-	 *         PATH : 액션 코드별 값  
+	 * @return HashMap RESULT : 성공 혹은 실패 MSG : 실패시 에러 메시지 PATH : 액션 코드별 값
 	 */
 	private HashMap<String, String> checkProperties(String appId, String actionCode) {
 		HashMap<String, String> retMap = new HashMap<String, String>();
-    	retMap.put("RESULT", "SUCCESS");
-        String[] ids = this.getPropertiesByKey(this.env, "apps.id");
-        
-        if(ids == null) {
-        	retMap.put("RESULT", "FAIL");
-        	retMap.put("MSG", this.getReturnMsg(2));
-        	return retMap;
-        }
-        
+		retMap.put("RESULT", "SUCCESS");
+		String[] ids = this.getPropertiesByKey(this.env, "apps.id");
+
+		if (ids == null) {
+			retMap.put("RESULT", "FAIL");
+			retMap.put("MSG", this.getReturnMsg(2));
+			return retMap;
+		}
+
 		boolean isExists = false;
-		for(String id : ids) {
-			if(id.equals(appId)) {
+		for (String id : ids) {
+			if (id.equals(appId)) {
 				isExists = true;
 				break;
 			}
 		}
-		
-		if(isExists == false) {
-        	retMap.put("RESULT", "FAIL");
-        	retMap.put("MSG", this.getReturnMsg(3));
-        	return retMap;
+
+		if (isExists == false) {
+			retMap.put("RESULT", "FAIL");
+			retMap.put("MSG", this.getReturnMsg(3));
+			return retMap;
 		}
-		
-		
-		if(actionCode.equals("info")) {
+
+		if (actionCode.equals("info")) {
 			String port = this.env.getProperty(appId + ".port");
-			
-			if(port == null || port.isEmpty()) {
-	        	retMap.put("RESULT", "FAIL");
-	        	retMap.put("MSG", this.getReturnMsg(5));
-	        	return retMap;
+
+			if (port == null || port.isEmpty()) {
+				retMap.put("RESULT", "FAIL");
+				retMap.put("MSG", this.getReturnMsg(5));
+				return retMap;
 			}
-			
+
 			retMap.put("PORT", port);
-			
+
 		} else {
 			String path = this.env.getProperty(appId + "." + actionCode + ".path");
-			
-			if(path == null || path.isEmpty()) {
-	        	retMap.put("RESULT", "FAIL");
-	        	retMap.put("MSG", this.getReturnMsg(4));
-	        	return retMap;
+
+			if (path == null || path.isEmpty()) {
+				retMap.put("RESULT", "FAIL");
+				retMap.put("MSG", this.getReturnMsg(4));
+				return retMap;
 			}
-			
+
 			retMap.put("PATH", path);
 		}
-		
+
 		return retMap;
 	}
 
 	/**
-	 * Properties 파일에서 key에 해당하는 데이터를 읽어 Array로 리턴 
+	 * Properties 파일에서 key에 해당하는 데이터를 읽어 Array로 리턴
 	 * 
-	 * @param _properties
+	 * @param props
 	 * @param key
 	 * @return String[]
 	 */
-	private String[] getPropertiesByKey(Environment _properties, String key) {
-		String readValue = _properties.getProperty(key);
-		
-		if(readValue == null) {
+	private String[] getPropertiesByKey(Environment props, String key) {
+		String readValue = props.getProperty(key);
+
+		if (readValue == null) {
 			return null;
 		}
-		
+
 		return readValue.split(",");
 	}
-	
+
 	/**
 	 * 에러 코드별 리턴 메시지를 리턴한다.
 	 * 
@@ -402,23 +536,23 @@ public class AgentController {
 	 */
 	private String getReturnMsg(int code) {
 		String msg = "";
-		
-		if(code == 1) {
+
+		if (code == 1) {
 			msg = "Can Not Read The Properties File!";
-			
-		} else if(code == 2) {
+
+		} else if (code == 2) {
 			msg = "Can Not Find Application ID Properties!";
-			
-		} else if(code == 3) {
+
+		} else if (code == 3) {
 			msg = "Wrong Application ID!";
-			
-		} else if(code == 4) {
+
+		} else if (code == 4) {
 			msg = "Can Not Find Application Batch Path!";
-			
-		} else if(code == 5) {
+
+		} else if (code == 5) {
 			msg = "Can Not Find Application Port No!";
 		}
-		
+
 		return msg;
 	}
 }
