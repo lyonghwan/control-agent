24a93b5e1ff69b6a3ff174f22c19e55c7ce0cd85
lyonghwan <longhuan@hatiolab.com>
2017-08-17 12:37:45 +0900

    Fix IDC 백업 로직

--- src/main/java/xyz/elidom/control/agent/rest/AgentController.java
+++ src/main/java/xyz/elidom/control/agent/rest/AgentController.java
@@ -374,7 +374,7 @@ public class AgentController {
 	 * @param domainId
 	 * @return
 	 */
-	@RequestMapping(value = "/apps/{app_id}/backup_server", method = RequestMethod.GET)
+	@RequestMapping(value = "/apps/backup_server", method = RequestMethod.POST)
 	public String backupServer(@PathVariable("domain_id") String domainId) throws Exception {
 		// 1. 동기화 서버 호출 
 		RestTemplate rest = new RestTemplate();
